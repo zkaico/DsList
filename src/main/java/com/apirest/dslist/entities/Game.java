33d1e990b748d809da42ad6ef7f5a178cdf409ee
zkaico <kaiquelimaferreira1@gmail.com>
2023-07-11 18:10:23 -0300

    properties, Game, ORM

---
+++ src/main/java/com/apirest/dslist/entities/Game.java
@@ -0,0 +1,132 @@
+package com.apirest.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private long id;
+	private String title;
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platform;
+	private Double score;
+	private String igmUrl;
+	private String shortDescription;
+	private String longDescription;
+	
+public Game(long id, String title, Integer year, String genre, String platform, Double score, String igmUrl,
+		String shortDescription, String longDescription) {
+	this.id = id;
+	this.title = title;
+	this.year = year;
+	this.genre = genre;
+	this.platform = platform;
+	this.score = score;
+	this.igmUrl = igmUrl;
+	this.shortDescription = shortDescription;
+	this.longDescription = longDescription;
+	}
+
+public long getId() {
+	return id;
+}
+
+public void setId(long id) {
+	this.id = id;
+}
+
+public String getTitle() {
+	return title;
+}
+
+public void setTitle(String title) {
+	this.title = title;
+}
+
+public Integer getYear() {
+	return year;
+}
+
+public void setYear(Integer year) {
+	this.year = year;
+}
+
+public String getGenre() {
+	return genre;
+}
+
+public void setGenre(String genre) {
+	this.genre = genre;
+}
+
+public String getPlatform() {
+	return platform;
+}
+
+public void setPlatform(String platform) {
+	this.platform = platform;
+}
+
+public Double getScore() {
+	return score;
+}
+
+public void setScore(Double score) {
+	this.score = score;
+}
+
+public String getIgmUrl() {
+	return igmUrl;
+}
+
+public void setIgmUrl(String igmUrl) {
+	this.igmUrl = igmUrl;
+}
+
+public String getShortDescription() {
+	return shortDescription;
+}
+
+public void setShortDescription(String shortDescription) {
+	this.shortDescription = shortDescription;
+}
+
+public String getLongDescription() {
+	return longDescription;
+}
+
+public void setLongDescription(String longDescription) {
+	this.longDescription = longDescription;
+	}
+
+@Override
+public int hashCode() {
+	return Objects.hash(id);
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Game other = (Game) obj;
+	return id == other.id;
+}
+
+
+
+}
